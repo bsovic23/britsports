7a85e130935557aec3baecb00a6a8ee40812a47e
Brit Sovic <bsovic@tulane.edu>
2022-09-16 20:56:49 -0400

    updated waiverwire page

--- assets/js/waiverWire.js
+++ assets/js/waiverWire.js
@@ -1,10 +1,15 @@
 // Waiver List
 waiverList = document.querySelector("#waiverList");
 
-// Button 
+// Button
+// Button Add
 addWaiverPlayerBtn = document.querySelector("#addWaiverPlayer");
-
 addWaiverPlayerBtn.addEventListener("click", waiverSubmit);
+// Button Save
+saveWaiverListBtn = document.querySelector("click", saveList());
+// Button Load
+LoadWaiverListBtn = document.querySelector("click", loadList());
+
 
 // Button Function 
 function waiverSubmit() {
@@ -28,13 +33,13 @@ function waiverSubmit() {
         divEl.appendChild(pEl);
         divEl.appendChild(p2El);
 
-        if (waiverPlayerAddEl[1] == "Qb") {
-            divEl.setAttribute("class", "waiverCardQb")
-        } else if (waiverPlayerAddEl[1] == "Rb") {
-            divEl.setAttribute("class", "waiverCardRb")
-        } else if (waiverPlayerAddEl[1] == "Te") {
-            divEl.setAttribute("class", "waiverCardTe")
-        } else divEl.setAttribute("class", "waiverCardWr")
+        if (waiverPlayerAddEl[1] == "QB") {
+            divEl.setAttribute("class", "waiverCardQb");
+        } else if (waiverPlayerAddEl[1] == "RB") {
+            divEl.setAttribute("class", "waiverCardRb");
+        } else if (waiverPlayerAddEl[1] == "TE") {
+            divEl.setAttribute("class", "waiverCardTe");
+        } else divEl.setAttribute("class", "waiverCardWr");
 
         waiverList.appendChild(divEl);
 
@@ -54,10 +59,14 @@ function waiverSubmit() {
 }
 
 // Commit current waiverList to local storage
+function saveList() {
+    console.log("save working");
+}
 
 // Pull current waiverList when page loaded
-
-
+function loadList() {
+    console.log("loading working");
+}
 
 
 
