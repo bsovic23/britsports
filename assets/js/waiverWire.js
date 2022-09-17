064a0a1be803d670d2617e6ac9ae331a65899862
Brit Sovic <bsovic@tulane.edu>
2022-09-16 20:30:41 -0400

    updated the waiverwire page

--- assets/js/waiverWire.js
+++ assets/js/waiverWire.js
@@ -9,26 +9,46 @@ addWaiverPlayerBtn.addEventListener("click", waiverSubmit);
 // Button Function 
 function waiverSubmit() {
 
+    function clearWaiver() {
+        document.getElementById("waiverName").value = "";
+        document.getElementById("waiverTeam").value = "";
+    }
+
     function addToWaiverList() {
         var divEl = document.createElement('div');
-        var h1El = document.createElement('h1');
+        var h3El = document.createElement('h3');
         var pEl = document.createElement('p');
+        var p2El = document.createElement('p');
 
-        h1El.textContent = "Player: " + waiverPlayerAddEl[0];
+        h3El.textContent = "Player: " + waiverPlayerAddEl[0];
         pEl.textContent = "Position: " + waiverPlayerAddEl[1];
+        p2El.textContent = "Team: " + waiverPlayerAddEl[2];
 
-        divEl.appendChild(h1El);
+        divEl.appendChild(h3El);
         divEl.appendChild(pEl);
+        divEl.appendChild(p2El);
+
+        if (waiverPlayerAddEl[1] == "Qb") {
+            divEl.setAttribute("class", "waiverCardQb")
+        } else if (waiverPlayerAddEl[1] == "Rb") {
+            divEl.setAttribute("class", "waiverCardRb")
+        } else if (waiverPlayerAddEl[1] == "Te") {
+            divEl.setAttribute("class", "waiverCardTe")
+        } else divEl.setAttribute("class", "waiverCardWr")
 
         waiverList.appendChild(divEl);
+
+        // Clears the waiver form
+        clearWaiver();
     }
 
     var waiverPlayerAddEl = "";
 
     playerNameEl = document.getElementById("waiverName").value;
     playerPositionEl = document.querySelector('input[name="waiverPosition"]:checked').value;
+    playerTeamEl = document.getElementById('waiverTeam').value;
 
-    waiverPlayerAddEl = [playerNameEl, playerPositionEl];
+    waiverPlayerAddEl = [playerNameEl, playerPositionEl, playerTeamEl];
 
    addToWaiverList(waiverPlayerAddEl);
 }
