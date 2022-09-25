2915aaae5432ba7c094060d32c7b54d2312fe754
Brit Sovic <bsovic@tulane.edu>
2022-09-25 15:35:53 -0400

    updated newUser funciton

--- assets/js/newUser.js
+++ assets/js/newUser.js
@@ -1,29 +1,31 @@
 // Grab the info from submit button
 
-submitBtnEl = document.getElementById('#newUserFormSubmit');
+var submitBtnEl = document.getElementById('#newUserFormSubmit');
 
-submitBtnEl.addEventListener('click', NewUser);
+
 
 // NewUser Information 
 
 function NewUser() {
 
-    newFirstNameEl = document.getElementById('#newFirstName').value();
-    newLastNameEl = document.getElementById('#newFirstName').value();
-    newEmailEl = document.getElementById('#newFirstName').value();
-    newUserEl = document.getElementById('#newFirstName').value();
-    newPassEl = document.getElementById('#newFirstName').value();
+    var newFirstNameEl = document.getElementById('#newFirstName').value();
+    var newLastNameEl = document.getElementById('#newLastName').value();
+    var newEmailEl = document.getElementById('#newEmail').value();
+    var newUserEl = document.getElementById('#newUsername').value();
+    var newPassEl = document.getElementById('#newPassword').value();
 
     var new_user = {
-        fn: newFirstNameEl,
-        ln: newLastNameEl,
-        email: newEmailEl,
-        user: newUserEl,
-        pass: newPassEl,
+        "fn": newFirstNameEl,
+        "ln": newLastNameEl,
+        "email": newEmailEl,
+        "user": newUserEl,
+        "pass": newPassEl,
     }
 
     return new_user;
-}
+};
+
+submitBtnEl.addEventListener("click", NewUser);
 
 console.log(NewUser());
 
